44e45be21e317122c209be2ce4db185beaff1c8f
Your Name <gilbertozioma0@gmail.com>
2023-08-17 21:50:40 +0100

    fixed-bottom

--- includes/footer-admin.php
+++ includes/footer-admin.php
@@ -1,4 +1,4 @@
 <!-- Footer -->
-<footer class="p-3  m-auto text-light text-center">
-    <p>All Rights Reserved. &copy; 2023 Designed by RG</p>
+<footer class="p-2 fixed-bottom">
+    <p class=" m-auto text-light text-center">All Rights Reserved. &copy; 2023 Designed by RG</p>
 </footer>
