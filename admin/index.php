89cbaa08719262519e814f55a6806a66377c466c
Your Name <gilbertozioma0@gmail.com>
2023-08-17 21:43:27 +0100

    Implemented a sidebar and analytical dashboard

--- admin/index.php
+++ admin/index.php
@@ -13,6 +13,7 @@ include('../functions/common_function.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashbord</title>
     <!-- Bootstrap CSS Link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../bootstrap-5/css/bootstrap.min.css">
     <!-- Font Awesome  Link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -33,30 +34,43 @@ include('../functions/common_function.php');
         }
 
         .imgg2 {
-            width: 80px;
-            height: 80px;
+            width: 40px;
+            height: 40px;
             object-fit: contain;
             border-radius: 100%;
         }
 
-        .pro {
-            height: 30%;
+        /* .pro {
+            height: 100vh !important;
 
+        } */
+
+        .sb-li a:hover {
+            color: #F79F1F !important;
+            /* border-bottom: 1px solid #F79F1F; */
         }
 
-        .p-li a:hover {
-            border-bottom: 1px solid #F79F1F;
+        .sb-li a {
+            transition: .1s !important;
+        }
+
+        .fs {
+            font-size: 13.5px;
+            margin-left: 53px !important;
+        }
+        .mt{
+            margin-top: -11px !important;
         }
     </style>
 </head>
 
 <body class="bg-secondary">
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light nav-bg">
+    <nav class="navbar navbar-expand-lg sticky-top navbar-light nav-bg">
         <div class="container-fluid">
-            <a href="#"><img class="logoo" src="../admin/product_images/Logo2.png" alt="logo"></a>
+            <a href="index.php"><img class="logoo" src="../admin/product_images/Logo2.png" alt="logo"></a>
             <nav class="navbar navbar-expand-lg">
-                <ul class="navbar-nav">
+                <div class="navbar-nav">
                     <?php
                     if (!isset($_SESSION['admin_name'])) {
                         echo "<li class='nav-item'> <a class='nav-link text-light' href='index.php'>Welcom Admin</a>
@@ -78,92 +92,133 @@ include('../functions/common_function.php');
                     }
                     ?>
 
-                </ul>
+                </div>
             </nav>
         </div>
     </nav>
-
-    <!-- Second child -->
-
-    <div class="bg-secondary">
-        <h3 class="text-center p-3 text-light">Manage Details</h3>
-
-    </div><br>
-
     <?php include('../includes/session.php'); ?>
 
-    <br>
-    <!-- Admin Dashbord -->
-    <div class="row bg-secondary">
-        <div class="col-md-2 bg-secondary sticky-top pro mb-4">
-            <div class="row border text-center border-secondary">
 
-                <div class="nav-item text-center p-2 nav-bg">
-                    <a class="nav-link text-light" href="#">
-                        <h4>Admin Profile</h4>
-                    </a>
+    <!-- Side bar -->
+    <div class="row bg-secondary mb-5">
+
+        <div class="col-md-2 bg-secondary border position-static border-secondary overflow-x-hidden pro">
+            <div class="row sticky text-center ">
+
+                <h4 class="bg-light">
+                </h4>
+                <div class=' d-flex m-auto p-2 p-li'>
+                    <?php
+                    $admin_name = $_SESSION['admin_name'];
+                    $admin_image = "SELECT * FROM tbl_admin WHERE admin_name = '$admin_name'";
+                    $admin_image = mysqli_query($conn, $admin_image);
+                    $fetch_image = mysqli_fetch_array($admin_image);
+                    $admin_image = $fetch_image['admin_image'];
+                    echo "<div class='m-auto'>
+                        <a>
+                        <img src='admin_images/$admin_image' alt='$admin_image' class='imgg2'>
+                        </a>
+                    </div>";
+                    ?>
                 </div>
-
-                <?php
-                $admin_name = $_SESSION['admin_name'];
-                $admin_image = "SELECT * FROM tbl_admin WHERE admin_name = '$admin_name'";
-                $admin_image = mysqli_query($conn, $admin_image);
-                $fetch_image = mysqli_fetch_array($admin_image);
-                $admin_image = $fetch_image['admin_image'];
-                echo "<div class='nav-item mb-1'><br>
-                    <img src='admin_images/$admin_image' alt='$admin_image' class='imgg2'>
-                </div>";
-
-                ?>
-                <div class='related2 p-li fs-5'>
+                <p class="">
                     <?php echo $admin_name = $_SESSION['admin_name'] ?>
+                </p>
+                <hr>
+
+            </div>
+            <div class="row">
+
+                <div class="accordion accordion-flush" id="accordionFlushExample">
+
+                    <p class="ms-5 dropdown-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        Add Products
+                    </p>
+                    <div id="collapseOne" class="accordion-collapse collapse mt-auto mt" data-bs-parent="#accordionFlushExample">
+                        <div class="ms-5 fs">
+                            <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="insert_category.php">Add Category</a></li>
+                            <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="insert_breakfast.php">Add Breakfast</a></li>
+                            <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="insert_lunch.php">Add Lunch</a></li>
+                            <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="insert_dinner.php">Add Dinner</a></li>
+                            <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="insert_carousel.php">Add Slider</a></li>
+                            <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="insert_chef.php">Add Chef</a></li>
+                        </div>
+                        <hr>
+                    </div>
+
+                    <div class="">
+                        <p class="ms-5 dropdown-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            View Products
+                        </p>
+                        <div id="collapseTwo" class="accordion-collapse collapse mt-auto mt" data-bs-parent="#accordionFlushExample">
+                            <div class="justify-content-start ms-5 fs">
+                                <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="index.php?view_products">View Products</a></li>
+                                <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="index.php?view_categories">View Categories</a></li>
+                                <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="index.php?view_breakfast">View Breakfast</a></li>
+                                <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="index.php?view_lunch">View Lunch</a></li>
+                                <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="index.php?view_dinner">View Dinner</a></li>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+
+                    <div class="">
+                        <p class="ms-5 dropdown-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                            View Items
+                        </p>
+                        <div id="collapseThree" class="accordion-collapse justify-content-start collapse mt-auto mt" data-bs-parent="#accordionFlushExample">
+                            <div class="ms-5 fs">
+                                <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="index.php?view_book">View Book</a></li>
+                                <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="index.php?view_carousel">View Slider</a></li>
+                                <li class="list-unstyled mb-2 sb-li"><a class="text-light" href="index.php?view_chef">View Chef</a></li>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                    <div class="ms-5">
+                        <li class="list-unstyled mb-2 sb-li"><a class="text-secondary" href="index.php?list_users">All Users</a></li>
+                        <li class="list-unstyled mb-2 sb-li"><a class="text-secondary" href="index.php?list_all_orders">All Orders</a></li>
+                        <li class="list-unstyled mb-2 sb-li"><a class="text-secondary" href="index.php?all_payments">All Payments</a></li>
+                    </div>
                 </div>
                 <hr>
 
-                <div class="nav-item p-li mb-3">
-                    <a class="text-decoration-none text-light" href="index.php?edit_admin_profile">Edit Account</a>
-                </div>
-                <div class="nav-item p-li mb-3">
-                    <a class="text-decoration-none text-light" href="index.php?delete_admin_profile">Delete Account</a>
-                </div>
-                <div class="nav-item p-li mb-3">
-                    <a class="text-decoration-none text-light" href="admin_logout.php">Logout</a>
-                </div>
+                <div class="ms-5 mb-3 mt-1">
 
+                    <div class="nav-item sb-li mb-3">
+                        <a class="text-decoration-none text-light" href="index.php?edit_admin_profile">Edit Account</a>
+                    </div>
+                    <div class="nav-item sb-li mb-3">
+                        <a class="text-decoration-none text-light" href="index.php?delete_admin_profile">Delete Account</a>
+                    </div>
+                    <div class="nav-item sb-li mb-3">
+                        <a class="text-decoration-none text-light" href="admin_logout.php">Logout</a>
+                    </div>
+                </div>
             </div>
         </div>
+        <!-- End Side bar -->
 
         <!-- Dashbord -->
-        <div class="col-md-10 bg-secondary w-75 p-0">
-
-            <div class="row ms-2">
-
-                <div class="text-center bg-secondary "><br>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-primary"><a href="index.php?insert_category" class="nav-link text-light my-2">Add Category</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-primary"><a href="insert_breakfast.php" class="nav-link text-light my-2">Add Breakfast</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-primary"><a href="insert_lunch.php" class="nav-link text-light my-2">Add Lunch</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-primary"><a href="insert_dinner.php" class="nav-link text-light my-2">Add Dinner</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-primary"><a href="insert_carousel.php" class="nav-link text-light my-2">Add Carousel</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-primary"><a href="insert_chef.php" class="nav-link text-light my-2">Add Chef</a></button><br>
-
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?view_products" class="nav-link text-dark my-2">View Products</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?view_categories" class="nav-link text-dark my-2">View Category</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?view_breakfast" class="nav-link text-dark m-1 my-2">View Breakfast</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?view_lunch" class="nav-link text-dark my-2">View Lunch</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?view_dinner" class="nav-link text-dark my-2">View Dinner</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?view_book" class="nav-link text-dark my-2">View Book</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?list_all_orders" class="nav-link text-dark my-2">All Orders</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?all_payments" class="nav-link text-dark my-2">All Payments</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?list_users" class="nav-link text-dark my-2">List Users</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?view_carousel" class="nav-link text-dark my-2">View Carousel</a></button>
-                    <button class="border-0 rounded-1 m-1 p-2 py-1 btn btn-info"><a href="index.php?view_chef" class="nav-link text-dark my-2">View Chef</a></button>
-                </div>
-            </div>
-
-            <!-- Fouth child -->
+        <div class="col-md-10 bg-secondary w-75 p-0 m-auto">
 
             <div class="container my-3">
                 <?php
+
+                // Display db_stat.php by default 
+                $showDefault = true;
+
+                // Check if any insert or view button is clicked
+                if (isset($_GET['insert_category']) || isset($_GET['insert_breakfast']) || isset($_GET['insert_lunch']) || isset($_GET['insert_dinner']) || isset($_GET['insert_chef']) || isset($_GET['insert_product']) || isset($_GET['insert_order']) || isset($_GET['insert_carousel']) ||  isset($_GET['view_categories']) || isset($_GET['view_products']) || isset($_GET['view_breakfast']) || isset($_GET['view_lunch']) || isset($_GET['view_dinner']) || isset($_GET['view_chef']) || isset($_GET['view_slider']) || isset($_GET['view_book']) || isset($_GET['view_carousel']) || isset($_GET['list_users']) || isset($_GET['list_all_orders']) || isset($_GET['all_payments'])) {
+
+                    $showDefault = false;
+                }
+
+                // Include db_stat.php if the default should be shown
+                if ($showDefault) {
+                    include('db_stat.php');
+                }
+
                 // Add Category
                 if (isset($_GET['insert_category'])) {
                     include('insert_category.php');
@@ -319,7 +374,7 @@ include('../functions/common_function.php');
     ?>
 
     <!-- Bootstrap js Link -->
-    <script src="../bootstrap-5/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
